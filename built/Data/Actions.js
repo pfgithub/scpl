fcd7fd365e4bb819f6a16169fb3b9f1009169b91
pfg <omegariisthebest@gmail.com>
2019-02-28 12:28:26 -0800

    Code: Remove gitignored files

--- built/Data/Actions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const deepmerge = require("deepmerge");
-const builtin = require("./Shortcuts 2.1.2.json");
-const overrides = require("./Overrides.json");
-const merged = deepmerge(builtin[0], overrides[0]);
-merged["is.workflow.actions.documentpicker.open"].Parameters[4].Hidden = true;
-exports.default = merged;
