cc1ccd3ae0c03b9b3cb7c58dfa0fced22be79bfa
pfg <omegariisthebest@gmail.com>
2019-08-25 22:16:50 -0700

    Test: Nicer parsestringoutput

--- test/parserstring.ts
+++ test/parserstring.ts
@@ -4,11 +4,13 @@ import { o } from "../src/ParserHelper";
 import "../src/ShortcutsParser"; // populate o
 
 test("tostring", () => {
-	let result = "";
+	const resultArr: [string, string][] = [];
 	Object.keys(o).forEach(v => {
 		const vq = resolve(resolveIn(o[v]));
-		result += `${vq.name}  =  ${vq.toString()}\n`;
+		resultArr.push([vq.name || "!!noname", vq.toString()]);
 	});
+	const longestElement = resultArr.reduce((t, c) => Math.max(t, c[0].length), 0);
+	const result = resultArr.map(q => `${q[0].padEnd(longestElement, " ")  }  =  ${  q[1]}`).join("\n");
 	const cont = fs.readFileSync(`${__dirname  }/parsestringoutput.baparse`, "utf-8");
 	fs.writeFileSync(`${__dirname  }/parsestringoutput.baparse`, result, "utf-8");
 	expect(result).toEqual(cont);
